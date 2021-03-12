5cf6f462c72ea474e21cbc7fef7bcb74121f6463
Kevin Bluer <kevin@bluer.com>
2021-03-12 13:24:11 -0600

    placeholder minting script

---
+++ scripts/mint.js
@@ -0,0 +1,5 @@
+const gallery = await MyGallery.deployed()
+gallery.mint(accounts[0], "QmS4t7rFPxaaNriXvCmALr5GYRAtya5urrDaZgkfHutdCG")
+gallery.mint(accounts[0], "QmXXtPjgeunQU6kxLdXJ2SDwu2TJtYjA9tHceQ5XtShkWo")
+gallery.mint(accounts[0], "QmejUVxERZi1hTKTxJfSyK5x5KWzSgSBPChjZGWPj9TALj")
+gallery.mint(accounts[0], "QmZYxFnayjhnEFddE2mrs563RjvZbQbrZ9hZKY1wCWMPCK")
